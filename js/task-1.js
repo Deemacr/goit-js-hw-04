8bdd7183a16396308e381b2bd8c5de0471f6e946
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-05-12 12:33:12 +0300

    HW-4 finished 3.0

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,18 @@
 const account = {
-    owner: 'Mango',
-    balance: 24000,
-    discount: 0.1,
-    orders: ['order-1', 'order-2', 'order-3'],
-    changeDiscount(value) {
-        this.discount = value;
-    },
-    showOrders() {
-        return this.orders;
-    },
-    addOrder(cost, order) {
-        this.balance -= cost;
-        this.orders.push(order);
-    },
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  showOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
 };
 
 account.changeDiscount(0.15);
--- js/task-2.js
+++ js/task-2.js
@@ -1,47 +1,31 @@
-// const inventory = {
-//     items: ['Knife', 'Gas mask'],
-
-//     add(itemName) {
-//         console.log(`Adding ${itemName} to inventory`);
-
-//         this.items.push(itemName);
-//     },
-//     remove(itemName) {
-//         console.log(`Removing ${itemName} from inventory`);
-//         this.items = this.items.filter(item => item !== itemName);
-//     },
-
-// };
-
 const inventory = {
-    items: ['Knife', 'Gas mask'],
-
-    add(itemName) {
-        console.log(`Adding ${itemName} to inventory`);
-        this.items.push(itemName);
-    },
-    remove(itemName) {
-        console.log(`Removing ${itemName} from inventory`);
-        this.items = this.items.filter(item => item !== itemName);
-    },
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+    // console.log(this)
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+    this.items = this.items.filter(item => item !== itemName);
+  },
 
 };
 
-
-const invokeInventoryAction = function (obj, itemName, action) {
-    console.log(`Invoking action on ${itemName}`);
-    action.call(obj, itemName);
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
 };
 
 
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
 
-invokeInventoryAction(inventory, 'Medkit', inventory.add);
 // Invoking action on Medkit
 // Adding Medkit to inventory
 
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
 
-invokeInventoryAction(inventory, 'Gas mask', inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 
